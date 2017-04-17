2a99e096b3a9daa2e49c1fcd8588b90e5e0142f4
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:44:32 +0200

    tb_rhs_op_titoli.kunduz mission added

---
+++ briefing_red.hpp
@@ -0,0 +1,17 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <HO> Hideouts<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base at Foxtrot unlocked upon capture of Delta<br />
+<RP> Team Rally Point at Foxtrot<br />
+<br />
+
+<titleFont>Strategies:</titleFont><br />
+<*> Have infantry ready to defend Delta before the enemy occupy it<br />
+<*> Attack Charlie to slow down the enemy advance<br />
+<br />
+
+<titleFont>Tips:</titleFont><br />
+<*> <yellow>Controlling the center is key to this mission</yellow>
